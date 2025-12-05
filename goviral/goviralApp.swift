75a324918a33e068c84ad0f17a24de2568d582e5
charvi bansal <charvibansal2006@gmail.com>
2025-12-04 19:00:42 -0500

    Initial Commit

---
+++ goviral/goviralApp.swift
@@ -0,0 +1,17 @@
+//
+//  goviralApp.swift
+//  goviral
+//
+//  Created by Charvi Bansal on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct goviralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
